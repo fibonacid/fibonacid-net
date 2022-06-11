2ca128ea1218faf06d8214d6ba5070bf76a8146e
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-06-12 00:56:53 +0200

    create gsap module

---
+++ lib/gsap.ts
@@ -0,0 +1,7 @@
+import gsap from "gsap";
+import _ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(_ScrollTrigger);
+
+export default gsap;
+export const ScrollTrigger = _ScrollTrigger;
