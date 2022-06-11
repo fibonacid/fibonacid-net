ead7da04bfeac673fb75bd815fa0717fe2146e53
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-06-11 17:21:57 +0200

    add margin

--- pages/friend.tsx
+++ pages/friend.tsx
@@ -1,14 +1,22 @@
 import { NextPage } from "next";
+import styled from "styled-components";
 
 const friends = ["Mark", "Tom"];
 
+const Container = styled.div`
+  display: flex;
+  margin: 1rem;
+`;
+
 const Friend: NextPage = () => {
   return (
-    <ul>
-      {friends.map((name, index) => (
-        <li key={index}>{name}</li>
-      ))}
-    </ul>
+    <Container>
+      <ul>
+        {friends.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+    </Container>
   );
 };
 
