f67b784c336036fa072ec5f87f9880d30b22fa98
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-06-11 23:59:09 +0200

    Revert "use session storage"
    This reverts commit 4d79f3210b569cf3a2e02d269f2ad83cba9b28ae.

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -3,11 +3,11 @@ import { useCallback, useEffect, useState } from "react";
 export const useUser = () => {
   const [username, setUsername] = useState<string | null>();
   useEffect(() => {
-    setUsername(window.sessionStorage.getItem("username"));
+    setUsername(window.localStorage.getItem("username"));
   }, []);
   const reset = useCallback((name: string) => {
     setUsername(name);
-    window.sessionStorage.setItem("username", name);
+    window.localStorage.setItem("username", name);
   }, []);
   return { name: username, reset };
 };
