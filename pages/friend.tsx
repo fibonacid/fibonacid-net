6f7321642ccef99cd28409b473f28c918f9c0607
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-06-11 17:35:41 +0200

    cleanup

--- pages/friend.tsx
+++ pages/friend.tsx
@@ -2,8 +2,6 @@ import { NextPage } from "next";
 import styled from "styled-components";
 import FriendList from "../components/FriendList";
 
-const friends = ["Mark", "Tom"];
-
 const Container = styled.div`
   display: flex;
   margin: 1rem;
