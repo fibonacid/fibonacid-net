36f7df51395b6b932232c403d4dabb4fc6212449
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-06-11 18:04:24 +0200

    add select state

--- components/FriendList.tsx
+++ components/FriendList.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import styled from "styled-components";
+import { useRouter } from "next/router";
+import styled, { css } from "styled-components";
 
 const Container = styled.nav`
   display: flex;
@@ -8,22 +9,30 @@ const Container = styled.nav`
   gap: 0.25rem;
 `;
 
-const Anchor = styled.a`
+const accent = css`
+  color: black;
+  background-color: ${({ theme }) => theme.colors.main};
+  &:nth-child(odd) {
+    background-color: ${({ theme }) => theme.colors.secondary};
+  }
+  border-color: ${({ theme }) => theme.colors.main};
+  &:nth-child(odd) {
+    border-color: ${({ theme }) => theme.colors.secondary};
+  }
+  transition: all 100ms;
+`;
+
+const Anchor = styled.a<{ $accent?: boolean }>`
   display: block;
   padding: 0.5rem 0.25rem 0.4rem 0.25rem;
   border: solid 1px;
   font-size: 0.75rem;
   text-align: center;
-  border-color: ${({ theme }) => theme.colors.main};
-  &:nth-child(odd) {
-    border-color: ${({ theme }) => theme.colors.secondary};
-  }
+
+  border-color: grey;
+  ${({ $accent }) => ($accent ? accent : "")}
   &:hover {
-    color: black;
-    background-color: ${({ theme }) => theme.colors.main};
-    &:nth-child(odd) {
-      background-color: ${({ theme }) => theme.colors.secondary};
-    }
+    ${accent}
   }
 `;
 
@@ -32,11 +41,12 @@ type Props = {
 };
 
 const FriendList: React.FC<Props> = ({ friends }) => {
+  const { query } = useRouter();
   return (
     <Container>
       {friends.map((name, index) => (
         <Link key={index} href={{ query: { n: name } }} shallow passHref>
-          <Anchor>{name}</Anchor>
+          <Anchor $accent={query.n === name}>{name}</Anchor>
         </Link>
       ))}
     </Container>
