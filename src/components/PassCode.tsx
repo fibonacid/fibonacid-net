04cf9c1cf8cdb3110c925d4550aabd5cc7c51512
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2024-02-10 19:30:04 +0100

    fix issues

--- src/components/PassCode.tsx
+++ src/components/PassCode.tsx
@@ -1,33 +1,78 @@
 "use client";
 
-import { ChangeEventHandler, useCallback, useState } from "react";
-import { flushSync } from "react-dom";
 import { useRouter } from "next/navigation";
+import { useCallback, useRef, useState } from "react";
+import { flushSync } from "react-dom";
+
+const PASSCODE = "hello";
 
 export default function PassCode() {
-  const [value, setValue] = useState("");
+  const container = useRef<HTMLFieldSetElement>(null);
+  const [values, setValues] = useState(["", "", "", "", ""]);
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
-  const handleChange = useCallback<ChangeEventHandler<HTMLInputElement>>(
-    (e) => {
-      const nextValue = e.target.value;
-      flushSync(() => {
-        setValue(nextValue);
-      });
-      if (nextValue === "hello") {
-        router.push(`/${nextValue}`);
-      }
-    },
-    [router],
-  );
+  const moveFocus = useCallback((index: number) => {
+    const input = container.current?.children[index] as
+      | HTMLInputElement
+      | undefined;
+    input?.focus();
+  }, []);
 
   return (
-    <input
-      type="password"
-      placeholder="Type hello to enter"
-      className="focus:ring-neutral-700 bg-neutral-950 text-neutral-50 rounded-sm border-none text-center"
-      onChange={handleChange}
-      value={value}
-    />
+    <fieldset ref={container} className="flex gap-2">
+      {PASSCODE.split("").map((_, index) => (
+        <input
+          key={index}
+          className="w-12 h-12 text-4xl text-center bg-neutral-950 rounded-sm"
+          value={values[index]}
+          disabled={loading}
+          onKeyDown={(e) => {
+            // Handle arrow keys
+            if (e.key === "ArrowRight") {
+              return moveFocus(index + 1);
+            }
+            if (e.key === "ArrowLeft") {
+              return moveFocus(index - 1);
+            }
+            if (e.key === "Backspace") {
+              // Remove the nearest value and move focus
+              const newValues = [...values];
+              if (newValues[index]) {
+                newValues[index] = "";
+              } else {
+                newValues[index - 1] = "";
+              }
+              setValues(newValues);
+              return moveFocus(index - 1);
+            }
+            // Ignore special characters
+            if (e.key.length > 1 || e.key === " ") {
+              return;
+            }
+            // Set the value of the input
+            const newValues = [...values];
+            newValues[index] = e.key;
+
+            flushSync(() => {
+              setValues(newValues);
+            });
+
+            const numValues = newValues.filter((v) => v).length;
+            if (numValues === PASSCODE.length) {
+              const entered = newValues.join("");
+              if (entered === PASSCODE) {
+                router.push("/hello");
+              } else {
+                setValues(["", "", "", "", ""]);
+                moveFocus(0);
+              }
+            } else {
+              moveFocus(index + 1);
+            }
+          }}
+        />
+      ))}
+    </fieldset>
   );
 }
