8175dc6e2f68fe71b1677992bc17844b32cff1ce
Lorenzo Rivosecchi <lorenzo.rivosecchi@botshelf.ai>
2024-02-10 16:43:12 +0100

    improve avatar quality

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -3,14 +3,14 @@ import avatar from "@/assets/avatar.jpg";
 
 export default function Avatar() {
   return (
-    <Image
-      src={avatar}
-      alt="Avatar"
-      width={160}
-      height={160}
-      placeholder="blur"
-      quality={100}
-      className="rounded-full overflow-hidden w-40 h-40 shadow-sm"
-    />
+    <div className="overflow-hidden relative w-40 h-40 rounded-full shadow-sm">
+      <Image
+        src={avatar}
+        alt="Avatar"
+        placeholder="blur"
+        quality={100}
+        layout="fill"
+      />
+    </div>
   );
 }
