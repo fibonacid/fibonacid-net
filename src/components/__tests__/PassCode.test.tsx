fa928d47c9ad498364e05c7fa56047ed887484d0
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2024-02-11 18:50:36 +0100

    add test

--- src/components/__tests__/PassCode.test.tsx
+++ src/components/__tests__/PassCode.test.tsx
@@ -42,3 +42,26 @@ test("accept a key and focuses next element", async () => {
     }
   }
 });
+
+test("deletes keys with backspace", async () => {
+  const { user } = setup();
+
+  for (let i = 0; i < NUMBER_OF_INPUTS; i++) {
+    const input = getInput(i);
+    const key = i.toString();
+
+    // focus input
+    await user.click(input);
+
+    // type key (moves focus)
+    await user.keyboard(key);
+    expect(input).toHaveValue(key);
+
+    // restore focus
+    await user.click(input);
+
+    // delete key
+    await user.keyboard("{Backspace}");
+    expect(input).not.toHaveValue(key);
+  }
+});
