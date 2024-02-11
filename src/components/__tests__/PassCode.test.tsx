d1848ea31d604fba43facd109f0e9d68dc87c4eb
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2024-02-11 15:56:00 +0100

    test user input

--- src/components/__tests__/PassCode.test.tsx
+++ src/components/__tests__/PassCode.test.tsx
@@ -1,6 +1,9 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import PassCode from "../PassCode";
+
+const NUMBER_OF_INPUTS = 5;
 
 function getInput(index: number) {
   return screen.getByLabelText(`Passcode input ${index + 1}`, {
@@ -8,9 +11,21 @@ function getInput(index: number) {
   });
 }
 
-test("renders 5 passcode inputs", () => {
+test(`renders ${NUMBER_OF_INPUTS} inputs`, () => {
   render(<PassCode />);
   for (let i = 0; i < 5; i++) {
     expect(getInput(i)).toBeInTheDocument();
+  }
+});
+
+test("accepts input", async () => {
+  const user = userEvent.setup();
+  render(<PassCode />);
+
+  for (let i = 0; i < 5; i++) {
+    const input = getInput(i);
+    const value = i.toString();
+    await user.type(input, value);
+    expect(input).toHaveValue(value);
   }
 });
