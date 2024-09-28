2a683083fc39d30f05d507a4a7462d0df16b2d88
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2024-09-28 17:44:18 +0200

    add button

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import { Button } from "@/components/Button";
+
 export default function Hello() {
   return (
     <main className="min-h-full grid gap-4 place-content-center">
-      <div>
-        <h1 className="text-2xl">Hello yourself!</h1>
+      <div className="text-center flex flex-col gap-5 justify-center items-center">
+        <h1 className="text-3xl">Hello yourself!</h1>
+        <p>To be honest, I don't care a lot about this website</p>
+        <Button onClick={() => alert("I'm sorry")}>I am dissapointed</Button>
       </div>
     </main>
   );
