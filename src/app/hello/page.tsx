9be4fdb60c4731e93e665bbf2e0f822ac5c3cb36
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2024-09-28 20:03:03 +0200

    improve mobile layout

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -31,7 +31,7 @@ const content: StepsProps["content"] = [
 export default function Hello() {
   return (
     <main className="min-h-full grid gap-4 place-content-center">
-      <header className="flex gap-3 absolute left-5 top-5 items-center">
+      <header className="absolute inset-x-5 top-5 flex gap-3 items-center">
         <Avatar className="w-10 h-10" />
         <h1 className="text-md text-white/50">fibonacid.net</h1>
       </header>
